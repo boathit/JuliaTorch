a99fa3479d731d21df68ad2f35d72665b6773dfb
boathit <xiucheng90@gmail.com>
2018-10-22 17:43:39 +0800

    update dataloader

--- dataloader.jl
+++ dataloader.jl
@@ -1,9 +1,9 @@
 using Random:randperm!
 
 """
-    DataLoader(data...; batchsize::Int=100, shuffle=true)
+    DataLoader(dataset...; batchsize::Int=100, shuffle=true)
 
-DataLoader provides iterators over the dataset (data...).
+DataLoader provides iterators over the dataset.
 
 ```julia
 X = rand(10, 1000)
@@ -19,25 +19,25 @@ Flux.train!(loss, trainloader, opt)
 ```
 """
 struct DataLoader
-    data::Tuple
+    dataset::Tuple
     batchsize::Int
     shuffle::Bool
     indices::Vector{Int}
     n::Int
 end
 
-function DataLoader(data::NTuple{N,AbstractArray}; batchsize::Int=100, shuffle=false) where N
-    l = last.(size.(data))
+function DataLoader(dataset::NTuple{N,AbstractArray}; batchsize::Int=100, shuffle=false) where N
+    l = last.(size.(dataset))
     n = first(l)
-    @assert all(n .== l) "The data should have the same length."
-    # n = last(size(first(data)))
+    all(n .== l) || throw(DimensionMismatch("All data should have the same length."))
+    # n = last(size(first(dataset)))
     indices = collect(1:n)
     shuffle && randperm!(indices)
-    DataLoader(data, batchsize, shuffle, indices, n)
+    DataLoader(dataset, batchsize, shuffle, indices, n)
 end
 
-DataLoader(data...; batchsize::Int=100, shuffle=false) =
-    DataLoader(data, batchsize=batchsize, shuffle=shuffle)
+DataLoader(dataset...; batchsize::Int=100, shuffle=false) =
+    DataLoader(dataset, batchsize=batchsize, shuffle=shuffle)
 
 function Base.iterate(it::DataLoader, start=1)
     if start > it.n
@@ -46,12 +46,12 @@ function Base.iterate(it::DataLoader, start=1)
     end
     nextstart = min(start + it.batchsize, it.n + 1)
     i = it.indices[start:nextstart-1]
-    element = Tuple(copy(selectdim(x, ndims(x), i)) for x in it.data)
+    element = Tuple(copy(selectdim(x, ndims(x), i)) for x in it.dataset)
     return element, nextstart
 end
 
 Base.length(it::DataLoader) = it.n
-Base.eltype(it::DataLoader) = typeof(it.data)
+Base.eltype(it::DataLoader) = typeof(it.dataset)
 
 # function batchselect(x::AbstractArray, i)
 #     inds = CartesianIndices(size(x)[1:end-1])
@@ -59,7 +59,7 @@ Base.eltype(it::DataLoader) = typeof(it.data)
 # end
 
 function Base.show(io::IO, it::DataLoader)
-    print(io, "DataLoader(Dataset size = $(it.n)")
+    print(io, "DataLoader(dataset size = $(it.n)")
     print(io, ", batchsize = $(it.batchsize), shuffle = $(it.shuffle)")
     print(io, ")")
 end
