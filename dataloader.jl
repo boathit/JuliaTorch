b8904a14cd293323c0e8ef99618fcb925201e37c
boathit <xiucheng90@gmail.com>
2018-10-22 12:52:15 +0800

    update dataloader.jl

--- dataloader.jl
+++ dataloader.jl
@@ -1,5 +1,24 @@
 using Random:randperm!
 
+
+"""
+    DataLoader(data...; batchsize::Int=100, shuffle=true)
+
+DataLoader provides iterators over the dataset (data...).
+
+```julia
+X = rand(10, 1000)
+Y = rand(1, 1000)
+
+m = Dense(10, 1)
+loss(x, y) = Flux.mse(m(x), y)
+opt = ADAM(params(m))
+
+trainloader = DataLoader(X, Y, batchsize=256, shuffle=true)
+
+Flux.train!(loss, trainloader, opt)
+```
+"""
 struct DataLoader
     data::Tuple
     batchsize::Int
@@ -8,31 +27,16 @@ struct DataLoader
     n::Int
 end
 
-function DataLoader(data::NTuple{N, AbstractArray}; batchsize::Int=100, shuffle=false) where N
+function DataLoader(data::NTuple{N,<:AbstractArray}; batchsize::Int=100, shuffle=false) where N
     lens = [last(size(x)) for x in data]
     n = first(lens)
-    @assert all(len -> len == n, lens) "The last dimension of all the Arrays should be equal"
+    @assert all(len -> len == n, lens) "The data should have the same length."
     # n = last(size(first(data)))
     indices = collect(1:n)
     shuffle && randperm!(indices)
     DataLoader(data, batchsize, shuffle, indices, n)
 end
 
-"""
-    DataLoader(data...; batchsize::Int=100, shuffle=true)
-
-DataLoader provides iterators over the dataset (data...).
-
-```julia
-xs = rand(10)
-ys = rand(2, 10)
-dataloader = DataLoader(xs, ys, batchsize=3, shuffle=true)
-
-for (x, y) in dataloader
-    @show x
-end
-```
-"""
 DataLoader(data...; batchsize::Int=100, shuffle=false) =
     DataLoader(data, batchsize=batchsize, shuffle=shuffle)
 
@@ -48,9 +52,15 @@ function Base.iterate(it::DataLoader, start=1)
 end
 
 Base.length(it::DataLoader) = it.n
-Base.eltype(it::DataLoader) = NTuple{length(it.data), AbstractArray}
+Base.eltype(it::DataLoader) = typeof(it.data)
 
 # function batchselect(x::AbstractArray, i)
 #     inds = CartesianIndices(size(x)[1:end-1])
 #     x[inds, i]
 # end
+
+function Base.show(io::IO, it::DataLoader)
+    print(io, "DataLoader(Dataset size = $(it.n)")
+    print(io, ", batchsize = $(it.batchsize), shuffle = $(it.shuffle)")
+    print(io, ")")
+end
